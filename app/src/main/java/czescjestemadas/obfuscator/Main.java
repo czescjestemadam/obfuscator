d6c33b0a68ab23cd3e4f8584969f9a8c3e497951
adas_acer <adambimek@gmail.com>
2025-05-31 15:29:28 +0200

    test: auto extracting

--- app/src/main/java/czescjestemadas/obfuscator/Main.java
+++ app/src/main/java/czescjestemadas/obfuscator/Main.java
@@ -1,10 +1,16 @@
 package czescjestemadas.obfuscator;
 
+import czescjestemadas.obfuscator.util.JarUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Map;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 @Slf4j
 public class Main
@@ -30,7 +36,51 @@ public class Main
 
 		final Obfuscator obf = new Obfuscator(settings);
 
-//		obf.obfuscate("run/km-items-0.1.10.3.jar", "run/out/km-items.jar");
-		obf.obfuscate(new File("test-jar/target/obfuscator-test-jar-0.1.jar"), Path.of("run/out/obfuscator-test-jar.jar"));
+		final Path outputFile = Path.of("run/out/obfuscator-test-jar.jar");
+		clearDirOf(outputFile);
+		obf.obfuscate(new File("test-jar/target/obfuscator-test-jar-0.1.jar"), outputFile);
+		extractJar(outputFile);
+	}
+
+	private static void extractJar(Path jarPath)
+	{
+		final Path jarDir = jarPath.getParent();
+
+		try
+		{
+			final Map<JarEntry, byte[]> entries = JarUtil.read(new JarFile(jarPath.toFile()));
+
+			entries.forEach((entry, bytes) -> {
+				try
+				{
+					final Path path = jarDir.resolve(entry.getName());
+					path.getParent().toFile().mkdirs();
+					Files.write(path, bytes);
+				}
+				catch (IOException e)
+				{
+					throw new RuntimeException(e);
+				}
+			});
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static void clearDirOf(Path outputFile)
+	{
+		clearDir(outputFile.getParent().toFile());
+	}
+
+	private static void clearDir(File dir)
+	{
+		for (File file : dir.listFiles())
+		{
+			if (file.isDirectory())
+				clearDir(file);
+			file.delete();
+		}
 	}
 }
